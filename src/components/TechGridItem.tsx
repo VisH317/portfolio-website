4f4077cc73feea251e85991929bb99b744863b13
Vis <vt201916384@gmail.com>
2023-04-01 16:39:46 -0400

    undoing framer motion (add later back)

--- src/components/TechGridItem.tsx
+++ src/components/TechGridItem.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
-import { motion } from 'framer-motion'
+// import { motion } from 'framer-motion'
 
 interface TechGridItemProps {
     children: React.ReactNode,
@@ -30,8 +30,8 @@ export default function TechGridItem(props: TechGridItemProps) {
         <div onMouseOver={mouseOver} onMouseOut={mouseOut} className={`w-full aspect-square overflow-hidden ${props.hidden ? "sm:hidden md:hidden lg:block xl:hidden" : ""}`}>
             <div className={`bg-${props.image} bg-center bg-contain bg-no-repeat p-5 rounded-lg w-full aspect-square duration-300`+val} style={{transformStyle: "preserve-3d", transform:r, backfaceVisibility: 'hidden'}}>
                 {props.children}
-            </motion.div>
-            <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} className="bg-slate-500 p-5 rounded-lg w-full aspect-square duration-300 relative -top-full" style={{transformStyle: "preserve-3d", transform:b, backfaceVisibility: 'hidden'}}></div>
-        </motion.div>
+            </div>
+            <div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} className="bg-slate-500 p-5 rounded-lg w-full aspect-square duration-300 relative -top-full" style={{transformStyle: "preserve-3d", transform:b, backfaceVisibility: 'hidden'}}></div>
+        </div>
     )
 }
